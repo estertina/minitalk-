a3b9f2a65d861a8f707e947b7423e1cba39d71b4
estertina <estert99@gmail.com>
2026-07-16 07:54:50 +0200

    typos

--- minitalk.h
+++ minitalk.h
@@ -6,14 +6,14 @@
 /*   By: esttina <esttina@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 17:00:34 by esttina           #+#    #+#             */
-/*   Updated: 2026/07/16 07:11:26 by esttina          ###   ########.fr       */
+/*   Updated: 2026/07/16 07:37:33 by esttina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# ifdef _DEFAULT_SOURCE
+# ifndef _DEFAULT_SOURCE
 #  define _DEFAULT_SOURCE
 # endif
 
