79c427c82176f5459a606645d163291578954ef0
estertina <estert99@gmail.com>
2026-07-16 07:17:12 +0200

    added ft_atoi and fixed norminette

--- client.c
+++ client.c
@@ -6,16 +6,42 @@
 /*   By: esttina <esttina@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 21:01:25 by esttina           #+#    #+#             */
-/*   Updated: 2026/03/16 00:40:41 by esttina          ###   ########.fr       */
+/*   Updated: 2026/07/16 07:06:47 by esttina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void send_bits(int pid, char c)
+int	ft_atoi(const char *str)
 {
-	int	bit_counter = 0;
-	
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+
+void	send_bits(int pid, char c)
+{
+	int	bit_counter;
+
+	bit_counter = 0;
 	while (bit_counter < 8)
 	{
 		if ((c >> bit_counter) & 1)
@@ -29,13 +55,12 @@ void send_bits(int pid, char c)
 
 int	main(int argc, char **argv)
 {
-	int pid;
-	int i;
+	int	pid;
+	int	i;
 
 	if (argc != 3)
 		return (1);
-	
-	pid = atoi(argv[1]);
+	pid = ft_atoi(argv[1]);
 	i = 0;
 	while (argv[2][i])
 	{
